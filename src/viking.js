b0753f1a448986443840b4e417a4635933c7e4ef
Manuel <m.a.c.p.90@gmail.com>
2017-10-09 13:16:58 -0400

    final touch

--- src/viking.js
+++ src/viking.js
@@ -1,6 +1,9 @@
 //------------------------------------------------------
 // SOLDIER
 //------------------------------------------------------
+
+
+
 function Soldier (healthArg, strengthArg) {
   this.health = healthArg;
   this.strength = strengthArg;
@@ -44,7 +47,19 @@ Viking.prototype.receiveDamage = function (damage) {
 Viking.prototype.battleCry = function () {
   return "Odin Owns You All!";
 };
+var vikingOne =(100,150);
 
+
+var vikingOne = (100,150);
+
+
+var vikingThree=(100,150);
+
+var vikingFour=(100,150);
+
+var vikingFive =(100,150);
+
+// console.log(vikingFive);
 
 //------------------------------------------------------
 // SAXON
@@ -129,3 +144,80 @@ War.prototype.showStatus = function () {
     return 'Vikings and Saxons are still in the thick of battle.';
   }
 };
+function attack (){
+
+}
+var warFirst = new War ();
+
+
+var vikingOne = new Viking ("luis",200 ,10);
+
+var vikingTwo = new Viking ("carlo",75 ,100);
+
+var vikingThree = new Viking ("pedro",75 ,100);
+
+var vikingFour = new Viking ("fernando",75 ,100);
+
+var VikingFive= new Viking ("gonzalo",75 ,100);
+
+warFirst.addViking (vikingOne);
+warFirst.addViking (vikingTwo);
+warFirst.addViking (vikingThree);
+warFirst.addViking (vikingFour);
+warFirst.addViking (vikingFive);
+
+
+ var saxonOne = new Saxon (75,100);
+ var saxonTwo = new Saxon (75 ,100);
+ var saxonThree = new Saxon (75 ,100);
+ var saxonFour = new Saxon (75 ,100);
+ var saxonFive = new Saxon (75 ,100);
+console.log (saxonOne);
+
+
+warFirst.addSaxon (saxonOne);
+warFirst.addSaxon (saxonTwo);
+warFirst.addSaxon (saxonThree);
+warFirst.addSaxon (saxonFour);
+warFirst.addSaxon (saxonFive);
+
+
+$(document).ready(function() {
+var refresh = function(){
+ $(".viking-box.1 .health").text("health" + vikingOne.health);
+ $(".viking-box.1 .strength").text("strength" + vikingOne.strength);
+ $(".viking-box.2 .health").text("health" + vikingTwo.health);
+ $(".viking-box.2 .strength").text("strength"+ vikingTwo.strength);
+ $(".viking-box.3 .health").text("health"+ vikingThree.health);
+ $(".viking-box.3 .strength").text("strength" + vikingThree.strength);
+ $(".viking-box.4 .health").text("health"+ vikingFour.health);
+ $(".viking-box.4 .strength").text("strength" + vikingFour.strength);
+ $(".viking-box.5 .health").text("health" + vikingFive.health);
+ $(".viking-box.5 .strength").text("strength"+ vikingFive.strength);
+
+ $(".saxon-box.1 .health").text("health" + saxonOne.health);
+ $(".saxon-box.1 .strength").text("strength" + saxonOne.strength);
+ $(".saxon-box.2 .health").text("health" + saxonTwo.health);
+ $(".saxon-box.2 .strength").text("strength"+ saxonTwo.strength);
+ $(".saxon-box.3 .health").text("health"+ saxonThree.health);
+ $(".saxon-box.3 .strength").text("strength" + saxonThree.strength);
+ $(".saxon-box.4 .health").text("health"+ saxonFour.health);
+ $(".saxon-box.4 .strength").text("strength" + saxonFour.strength);
+ $(".saxon-box.5 .health").text("health" + saxonFive.health);
+ $(".saxon-box.5 .strength").text("strength"+ saxonFive.strength);
+};
+
+$(".viking-attack").on("click",function(){
+  warFirst.vikingAttack();
+  refresh();
+});
+$(".saxon-attack").on("click", function(){
+  warFirst.saxonAttack();
+  refresh();
+});
+
+
+
+
+
+});
